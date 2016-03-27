6503f302862cd2f8f58e5143a5e4df92f04b877a
Tan Si Nan <tansinan_1995@163.com>
2016-03-27 10:28:45 +0800

    Add the android bluetooth library "AndroidSmoothBluetooth".

--- AndroidApp/app/src/main/java/com/ledpainter/ledpainter/MainActivity.java
+++ AndroidApp/app/src/main/java/com/ledpainter/ledpainter/MainActivity.java
@@ -13,10 +13,48 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ToggleButton;
+import io.palaima.smoothbluetooth.Device;
+import io.palaima.smoothbluetooth.SmoothBluetooth;
 
 import com.rarepebble.colorpicker.ColorPickerView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SmoothBluetooth.Listener mListener = new SmoothBluetooth.Listener() {
+        @Override
+        public void onBluetoothNotSupported() {
+            //device does not support bluetooth
+        }
+
+        @Override
+        public void onBluetoothNotEnabled() {}
+
+        @Override
+        public void onConnecting(Device device) {}
+
+        @Override
+        public void onConnected(Device device) {
+        }
+
+        @Override public void onDisconnected() {}
+
+        @Override
+        public void onConnectionFailed(Device device) {
+        }
+
+        @Override public void onDiscoveryStarted() {}
+
+        @Override public void onDiscoveryFinished() {}
+
+        @Override public void onNoDevicesFound() {}
+
+        @Override public void onDevicesFound(List<Device> deviceList, SmoothBluetooth.ConnectionCallback connectionCallback) {}
+
+        @Override
+        public void onDataReceived(int data) {}
+    };
 
     protected ToggleButton buttonToggleBrush;
     protected ToggleButton buttonToggleErase;
