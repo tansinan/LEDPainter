a8e0fc8e709e70149ca051909aabdf380b7ca1ad
Tan Si Nan <tansinan_1995@163.com>
2016-04-02 11:46:21 +0800

    Implement basic phone-arduino bluetooth communication.

--- AndroidApp/app/src/main/java/com/ledpainter/ledpainter/MainActivity.java
+++ AndroidApp/app/src/main/java/com/ledpainter/ledpainter/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ledpainter.ledpainter;
 
 import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +14,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 import io.palaima.smoothbluetooth.Device;
 import io.palaima.smoothbluetooth.SmoothBluetooth;
@@ -21,15 +24,20 @@ import com.rarepebble.colorpicker.ColorPickerView;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    final static int REQUEST_ENABLE_BLUETOOTH = 100;
 
-    private SmoothBluetooth.Listener mListener = new SmoothBluetooth.Listener() {
+    private SmoothBluetooth.Listener bluetoothListener = new SmoothBluetooth.Listener() {
         @Override
         public void onBluetoothNotSupported() {
-            //device does not support bluetooth
+            Toast.makeText(MainActivity.this,
+                    "Sorry, this device doesn't support bluetooth.",Toast.LENGTH_SHORT).show();
         }
 
         @Override
-        public void onBluetoothNotEnabled() {}
+        public void onBluetoothNotEnabled() {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            MainActivity.this.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BLUETOOTH);
+        }
 
         @Override
         public void onConnecting(Device device) {}
@@ -50,7 +58,22 @@ public class MainActivity extends AppCompatActivity {
 
         @Override public void onNoDevicesFound() {}
 
-        @Override public void onDevicesFound(List<Device> deviceList, SmoothBluetooth.ConnectionCallback connectionCallback) {}
+        @Override public void onDevicesFound(List<Device> deviceList, SmoothBluetooth.ConnectionCallback connectionCallback) {
+            Device validDevice = null;
+            for(Device device : deviceList) {
+                if(device.getAddress().contains("98:D3:31:20:4D:C9")) {
+                    Toast.makeText(MainActivity.this, "Device found!", Toast.LENGTH_SHORT).show();
+                    validDevice = device;
+                    bluetoothManager.cancelDiscovery();
+                }
+            }
+            if(validDevice != null) {
+                connectionCallback.connectTo(validDevice);
+            }
+            else {
+                Toast.makeText(MainActivity.this, "No device found!", Toast.LENGTH_SHORT).show();
+            }
+        }
 
         @Override
         public void onDataReceived(int data) {}
@@ -60,10 +83,14 @@ public class MainActivity extends AppCompatActivity {
     protected ToggleButton buttonToggleErase;
     protected Button buttonSetColor;
     protected ToggleButton buttonTogglePreview;
+    protected Button buttonConnect;
+    protected Button buttonPushImage;
 
     protected PaintingView paintingView;
     protected ColorPickerView colorPickerView;
     protected AlertDialog colorPickerDialog;
+
+    protected SmoothBluetooth bluetoothManager;
 
     protected void setupColorPickerDialog() {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
@@ -101,6 +128,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        bluetoothManager = new SmoothBluetooth(this,
+                SmoothBluetooth.ConnectionTo.OTHER_DEVICE,
+                SmoothBluetooth.Connection.INSECURE, bluetoothListener);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -109,6 +140,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
         buttonToggleBrush = (ToggleButton)findViewById(R.id.button_toggle_brush);
         buttonToggleBrush.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -141,7 +173,20 @@ public class MainActivity extends AppCompatActivity {
                 paintingView.setPixelModeEnabled(buttonTogglePreview.isChecked());
             }
         });
-
+        buttonConnect = (Button)findViewById(R.id.button_connect);
+        buttonConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.onButtonConnect();
+            }
+        });
+        buttonPushImage = (Button)findViewById(R.id.button_push_image);
+        buttonPushImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.onButtonPushImage();
+            }
+        });
         paintingView = (PaintingView)findViewById(R.id.view);
         colorPickerView = new ColorPickerView(this);
         colorPickerView.setColor(Color.BLACK);
@@ -170,9 +215,36 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data)
+    {
+        switch (requestCode)
+        {
+            //Handles the result of the bluetooth intent
+            case REQUEST_ENABLE_BLUETOOTH:
+                if(resultCode == RESULT_OK) {
+                    bluetoothManager.doDiscovery();
+                }
+                break;
+        }
+    }
+
     public void onButtonSetColorClick() {
         colorPickerDialog.show();
         buttonSetColor.setTextColor(paintingView.brushColor);
         buttonSetColor.invalidate();
+    }
+
+    public void onButtonConnect() {
+        bluetoothManager.doDiscovery();
+    }
+
+    public void onButtonPushImage() {
+        if(bluetoothManager.isConnected()) {
+            for(int i = 0; i < 3; i++) {
+                int val = (int)(Math.random() * 255);
+                bluetoothManager.send(Integer.toString(val) + " ");
+            }
+        }
     }
 }
